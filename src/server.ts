c1020072f98f415fa931106811f78e8a7b89e506
Arthur <arthuromp000@gmail.com>
2021-07-29 16:08:54 -0300

    Add src folder with server file

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'retorno' }) 
+});
+
+app.listen(3333);
