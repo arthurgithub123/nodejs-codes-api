a950c776fe40e0385502a8d025969437e6118835
Arthur <arthuromp000@gmail.com>
2021-07-30 12:07:56 -0300

    Add routes to server configuration file

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ import './database';
 
 import './shared/dependencyInjectionContainer';
 
+import { router } from "./routes";
+
 const app = express();
 
 app.use(express.json());
+
+app.use(router);
 
 app.listen(3333);
