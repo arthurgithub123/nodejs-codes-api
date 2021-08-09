28ec71c2e8148f0864259c01d0bb10d3a02f29be
Arthur <arthuromp000@gmail.com>
2021-08-09 14:08:39 -0300

    Add findByEmail to UsersRepository

--- src/repositories/interfaces/IUsersRepository.ts
+++ src/repositories/interfaces/IUsersRepository.ts
@@ -1,7 +1,10 @@
 import { ICreateUserDTO } from "../../models/dtos/ICreateUserDTO";
 
+import { User } from "../../models/entities/User";
+
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
