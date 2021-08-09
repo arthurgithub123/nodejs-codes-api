9fae06bc5528c9794e142033c625aa15419b4176
Arthur <arthuromp000@gmail.com>
2021-08-09 14:14:23 -0300

    Add RolesRepository to dependency injection container

--- src/shared/dependencyInjectionContainer/index.ts
+++ src/shared/dependencyInjectionContainer/index.ts
@@ -3,4 +3,8 @@ import { container } from "tsyringe";
 import { IUsersRepository } from "../../repositories/interfaces/IUsersRepository";
 import { UsersRepository } from "../../repositories/implementations/UsersReposirory";
 
+import { IRolesRepository } from "../../repositories/interfaces/IRolesRepository";
+import { RolesRepository } from "../../repositories/implementations/RolesRepository";
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+container.registerSingleton<IRolesRepository>('RolesRepository', RolesRepository);
