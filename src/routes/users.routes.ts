e559275cfc6b52077653e9a8ea911fd5f7bd322b
Arthur <arthuromp000@gmail.com>
2021-08-11 15:12:07 -0300

    Add administrator creation to usersRouter

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../controllers/CreateUserController";
+import { CreateAdministratorController } from "../controllers/CreateAdministratorController";
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
+const createAdministratorController = new CreateAdministratorController();
 
-usersRouter.post('/', createUserController.handle);
 usersRouter.post('/user', createUserController.handle);
+usersRouter.post('/administrator', createAdministratorController.handle);
 
 export { usersRouter };
