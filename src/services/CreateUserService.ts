777ad934ccc4861daf425168603c6a4a110ac2c9
Arthur <arthuromp000@gmail.com>
2021-08-12 13:08:09 -0300

    Add error logic for same email creation

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -4,11 +4,19 @@ import { IUsersRepository } from "../repositories/interfaces/IUsersRepository";
 import { ICreateUserDTO } from "../models/dtos/ICreateUserDTO";
 import { hash } from "bcryptjs";
 
+import { GlobalErrorModel } from "../globalErrorHandling/GlobalErrorModel";
+
 @injectable()
 class CreateUserService {
   constructor(@inject("UsersRepository") private usersRepository: IUsersRepository) { }
 
   async execute({ name, email, password, role }: ICreateUserDTO): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if(user) {
+      throw new GlobalErrorModel('Já existe um usuário com esse e-mail');
+    }
+
     const passwordHash = await hash(password, 9);
 
     await this.usersRepository.create({
