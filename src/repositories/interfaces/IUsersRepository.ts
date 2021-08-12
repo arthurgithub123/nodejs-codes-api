e253191b039b1d753bee8087c778122e94421ac0
Arthur <arthuromp000@gmail.com>
2021-08-12 13:04:42 -0300

    Add findById to user repository

--- src/repositories/interfaces/IUsersRepository.ts
+++ src/repositories/interfaces/IUsersRepository.ts
@@ -5,6 +5,7 @@ import { User } from "../../models/entities/User";
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
