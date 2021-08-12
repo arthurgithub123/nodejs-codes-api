b73417c9a8bf279ba75dc47d1b3caa1ed813b2f8
Arthur <arthuromp000@gmail.com>
2021-08-12 14:07:53 -0300

    Add authentication and authorization to administrators creation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,12 +3,15 @@ import { Router } from "express";
 import { CreateUserController } from "../controllers/CreateUserController";
 import { CreateAdministratorController } from "../controllers/CreateAdministratorController";
 
+import { AuthenticationMiddleware } from "../middlewares/AuthenticationMiddleware";
+import { AdministratorAuthorizationMiddleware } from "../middlewares/AdministratorAuthorizationMiddleware";
+
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 const createAdministratorController = new CreateAdministratorController();
 
 usersRouter.post('/user', createUserController.handle);
-usersRouter.post('/administrator', createAdministratorController.handle);
+usersRouter.post('/administrator', AuthenticationMiddleware, AdministratorAuthorizationMiddleware, createAdministratorController.handle);
 
 export { usersRouter };
