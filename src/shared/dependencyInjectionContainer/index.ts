4bcb8b83cf619ffe4781e673180b1481c50dea29
Arthur <arthuromp000@gmail.com>
2021-08-20 12:04:44 -0300

    Add UserTokensRepository to dependency injection container

--- src/shared/dependencyInjectionContainer/index.ts
+++ src/shared/dependencyInjectionContainer/index.ts
@@ -6,5 +6,9 @@ import { UsersRepository } from "../../repositories/implementations/UsersReposir
 import { IRolesRepository } from "../../repositories/interfaces/IRolesRepository";
 import { RolesRepository } from "../../repositories/implementations/RolesRepository";
 
+import { IUserTokensRepository } from "../../repositories/interfaces/IUserTokensRepository";
+import { UserTokensRepository } from "../../repositories/implementations/UserTokensRepository";
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 container.registerSingleton<IRolesRepository>('RolesRepository', RolesRepository);
+container.registerSingleton<IUserTokensRepository>("UserTokensRepository", UserTokensRepository);
